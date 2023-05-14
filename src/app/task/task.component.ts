054bc43433fb36bbfb18c521d5c9ba2551b84a90
Ketan Desai <desai.ketan@gmail.com>
2023-05-14 17:05:15 -0400

    first commit

---
+++ src/app/task/task.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent {
+
+  @Input() task:any;
+
+  @Output()
+  onPinTask = new EventEmitter<Event>();
+
+  @Output()
+  onArchiveTask = new EventEmitter<Event>();
+  
+  
+}
